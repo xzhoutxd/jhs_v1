db4bc4d01043e71d83c39ad94ba33de85d87eea7
xin <zhouxinasl@gmail.com>
2015-07-21 13:51:26 +0800

    add timeout max time

--- jhs/base/Crawler.py
+++ jhs/base/Crawler.py
@@ -12,10 +12,10 @@ class Crawler:
     '''A class to crawl web pages'''
     def __init__(self):
         # Socket超时时间
-        socket.setdefaulttimeout(35)
+        socket.setdefaulttimeout(45)
 
         # http get超时时间
-        self.timeout     = (10, 30)
+        self.timeout     = (20, 40)
 
         # 网页编码
         self.f_coder     = 'gbk'
